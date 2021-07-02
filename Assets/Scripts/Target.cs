ea2f2056f4cf5ec629aaa1b21495cc7a88c3a77f
Leandro Chernij <lchernij@gmail.com>
2021-07-02 09:07:42 -0300

    Lesson 5.2 - Keeping Score

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
-
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -2;
+
+    // Score Update
+    private GameManager gameManager;
+    public int pointValue;
+
+    // Particle system
+    public ParticleSystem explosionParticle;
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         targetRb = GetComponent<Rigidbody>();
         // To jump on screen
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
@@ -46,6 +53,8 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
